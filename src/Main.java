9ddd1a25ca54d65bf49af66fb426473870074702
Joseph Raymund Ogapong <jogapong@gmail.com>
2025-05-02 09:00:59 +0800

    established connection with the Bisection button to open BisectionUI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
     public static void main(String[] args) {
         MenuGUI menuGUI = new MenuGUI();
-        BisectionUI bisectionUI = new BisectionUI();
+
+        menuGUI.bisection.addActionListener(new ActionListener() {
+            BisectionUI bisectionUI;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                this.bisectionUI = new BisectionUI();
+                menuGUI.dispose();
+            }
+        });
     }
 }
