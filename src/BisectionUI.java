00edca0b2acd3672296dc338ba237de982d332d0
Joseph Raymund Ogapong <jogapong@gmail.com>
2025-05-02 08:55:38 +0800

    added attributes for the BisectionUI

--- src/BisectionUI.java
+++ src/BisectionUI.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class BisectionUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class BisectionUI extends JFrame {
+    JToggleButton two_decimal, three_decimal, four_decimal;
+    JTable tableContent;
+    JLabel x0, x1, terminating_condition;
+    JTextField x0_field, x1_field, terminating_condition_field;
+    Container container;
+    BorderLayout layout;
+
+    public BisectionUI() throws HeadlessException {
+        
+    }
 }
